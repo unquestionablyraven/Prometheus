cb484023c573c13be0fc892e8ad2bfbed640894f
Raven <rae.vyn1914@gmail.com>
2022-04-09 19:45:25 +0300

    Responsive Course Page

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,62 +1,48 @@
 <x-guest-layout>
     <div class="p-10">
-        <div class="w-full flex justify-between items-center">
-            <h1 class="font-sans font-semibold text-lg text-gray-900">Prometheus</h1>
+        <div class="mx-auto w-full max-w-4xl xl:max-w-5xl 2xl:max-w-7xl flex justify-between items-baseline">
+            <h1 class="font-sans font-semibold text-lg xl:text-xl 2xl:text-2xl text-gray-900">Prometheus</h1>
 
-            <a href="" class="font-sans font-semibold text-lg text-gray-900"><span class="fas fa-bars"></span></a>
-        </div>
-
-        <div class="mt-12 w-full flex justify-between items-center">
-            <h1 class="font-sans font-bold text-xl sm:text-2xl text-gray-900">Ordinary Level<span class="font-black text-2xl sm:text-3xl text-gray-900"><br>Biology</span></h1>
-
-            <img class="inline rounded-full w-12 sm:w-24 object-cover" src="https://ui-avatars.com/api/?name=R&color=7F9CF5&background=EBF4FF" alt="Dr. John Doe's profile picture.">
-        </div>
-
-        <div class="mt-2 w-full flex justify-between items-baseline">
-            <h2 class="font-sans font-bold text-base sm:text-lg text-gray-700">Dr. John Doe</h2>
-
-            <h3 class="font-sans font-medium text-xs sm:text-sm text-gray-700">75/125 <span class="font-light text-xs text-gray-500 uppercase">Seats Taken</span></h3>
-        </div>
-
-        <div class="mt-6 w-full flex flex-col">
-            <h3 class="font-sans font-medium text-xs text-gray-700 uppercase">November <span class="font-light text-gray-500">2022</span></h3>
-            <h3 class="font-sans font-normal text-xs text-gray-700">Cambridge Assessment International Examinations</h3>
-        </div>
-
-        <div class="mt-10 w-full flex flex-col bg-gray-100 px-4 py-4 rounded">
-            <h3 class="font-sans font-semibold text-xs text-gray-900 uppercase">Group One</h3>
-
-            <div class="mt-4 grid grid-cols-8 gap-y-1 items-baseline">
-                <span class="col-span-2 font-mono font-light text-xs text-gray-500 mr-2">12:30 PM</span>
-                <h4 class="col-span-6 font-sans font-medium text-sm text-gray-700">Sunday</h4>
-
-                <span class="col-span-2 font-mono font-light text-xs text-gray-500 mr-2">10 PM</span>
-                <h4 class="col-span-6 font-sans font-medium text-sm text-gray-700">Wednesday</h4>
+            <div class="hidden md:flex justify-between items-center space-x-8">
+                <a href="" class="font-sans font-medium text-sm xl:text-base 2xl:text-lg text-gray-900">Courses</a>
+                <a href="" class="font-sans font-medium text-sm xl:text-base 2xl:text-lg text-gray-900">Tutors</a>
+                <a href="" class="font-sans font-medium text-sm xl:text-base 2xl:text-lg text-gray-900">Materials</a>
             </div>
 
-            <a class="mt-4 px-4 py-2 bg-gray-900 border border-transparent rounded font-sans font-semibold text-xs text-gray-50 text-center tracking-widest uppercase hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 transition">Enroll</a>
+            <a href="" class="md:hidden font-sans font-semibold text-lg text-gray-900"><span class="fas fa-bars"></span></a>
         </div>
 
-        <div class="mt-6 w-full flex flex-col bg-gray-100 px-4 py-4 rounded">
-            <h3 class="font-sans font-semibold text-xs text-gray-900 uppercase">Group Two</h3>
+        <div class="md:px-12 mx-auto mt-12 lg:mt-20 xl:mt-24 2xl:mt-28 w-full max-w-2xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-5xl grid grid-cols-12 items-baseline">
+            <h1 class="col-span-8 font-sans font-bold text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-gray-900">Ordinary Level<span class="font-black text-2xl sm:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-gray-900"><br>Biology</span></h1>
 
-            <div class="mt-4 grid grid-cols-8 gap-y-1 items-baseline">
-                <span class="col-span-2 font-mono font-light text-xs text-gray-500 mr-2">12:30 PM</span>
-                <h4 class="col-span-6 font-sans font-medium text-sm text-gray-700">Wednesday</h4>
+            <img class="col-span-4 sm:row-span-2 justify-self-end inline rounded-full w-12 sm:w-24 lg:w-28 xl:w-32 2xl:w-36 object-cover" src="https://ui-avatars.com/api/?name=R&color=7F9CF5&background=EBF4FF" alt="Dr. John Doe's profile picture.">
+
+            <h2 class="col-span-8 mt-2 font-sans font-bold text-base sm:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl text-gray-700">Dr. John Doe</h2>
+
+            <h3 class="col-span-4 sm:row-span-1 sm:row-start-3 sm:col-start-9 sm:self-end text-right font-sans font-medium text-xs sm:text-sm lg:text-base xl:text-lg 2xl:text-xl text-gray-700">75/125 <span class="font-light text-xs lg:text-sm xl:text-base 2xl:text-lg text-gray-500 uppercase">Seats Taken</span></h3>
+
+            <div class="col-span-12 sm:col-span-8 mt-6 w-full flex flex-col">
+                <h3 class="font-sans font-medium text-xs lg:text-sm xl:text-base 2xl:text-lg text-gray-700 uppercase">November <span class="font-light text-gray-500">2022</span></h3>
+                <h3 class="font-sans font-normal text-xs lg:text-sm xl:text-base 2xl:text-lg text-gray-700">Cambridge Assessment International Examinations</h3>
             </div>
-
-            <a class="mt-4 px-4 py-2 bg-gray-900 border border-transparent rounded font-sans font-semibold text-xs text-gray-50 text-center tracking-widest uppercase hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 transition">Enroll</a>
         </div>
 
-        <div class="mt-6 w-full flex flex-col bg-gray-100 px-4 py-4 rounded">
-            <h3 class="font-sans font-semibold text-xs text-gray-900 uppercase">Group Three</h3>
+        <div class="md:px-12 mx-auto mt-10 lg:mt-16 xl:mt-18 2xl:mt-24 w-full max-w-2xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-5xl grid grid-cols-1 gap-y-6">
+            <div class="w-full flex flex-col sm:flex-row sm:justify-between sm:items-center bg-gray-100 p-4 sm:px-8 sm:py-6 xl:px-10 xl:py-8 2xl:px-12 2xl:py-10 rounded">
+                <div class="flex flex-col">
+                    <h3 class="font-sans font-semibold text-xs lg:text-sm xl:text-base 2xl:text-lg text-gray-900 uppercase">Group One</h3>
 
-            <div class="mt-4 grid grid-cols-8 gap-y-1 items-baseline">
-                <span class="col-span-2 font-mono font-light text-xs text-gray-500 mr-2">11 PM</span>
-                <h4 class="col-span-6 font-sans font-medium text-sm text-gray-700">Saturday</h4>
+                    <div class="mt-4 w-fit grid grid-cols-2 gap-y-1 items-baseline">
+                        <h5 class="font-mono font-light text-xs lg:text-sm xl:text-base 2xl:text-lg text-gray-500 mr-2">12:30 PM</h5>
+                        <h4 class="font-sans font-medium text-sm lg:text-base xl:text-lg 2xl:text-xl text-gray-700">Sunday</h4>
+
+                        <h5 class="font-mono font-light text-xs lg:text-sm xl:text-base 2xl:text-lg text-gray-500 mr-2">10 PM</h5>
+                        <h4 class="font-sans font-medium text-sm lg:text-base xl:text-lg 2xl:text-xl text-gray-700">Wednesday</h4>
+                    </div>
+                </div>
+
+                <a class="mt-4 sm:mt-0 px-10 py-2 lg:px-12 xl:px-16 2xl:px-18 bg-gray-900 border border-transparent rounded font-sans font-semibold text-xs lg:text-sm xl:text-base 2xl:text-lg text-gray-50 text-center tracking-widest uppercase hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 transition">Enroll</a>
             </div>
-
-            <a class="mt-4 px-4 py-2 bg-gray-900 border border-transparent rounded font-sans font-semibold text-xs text-gray-50 text-center tracking-widest uppercase hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 transition">Enroll</a>
         </div>
     </div>
 </x-guest-layout>
